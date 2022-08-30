11824c022c5a4f52ca57d47f56f74f8dd62c60e1
israeel <israeel.abid@gmail.com>
2022-08-30 00:01:15 -0700

    this is the second commit

---
+++ my first javascript.js
@@ -0,0 +1,121 @@
+
+
+
+
+// var x = 10;
+// if(x==10){
+  
+//      document.write('x== 10')
+// }
+// else{
+//     document.write('x !=10')
+// }
+
+// var  Name="sharif"; 
+// var  fName="shirzad";
+
+
+// document.write('FName');
+
+
+// // this new 
+// let Fname ="asee";
+// LET LName = "nasarti";
+
+
+// Let = sName = "fayaz";
+
+// var Fname = "fayaz-nasarti"
+
+// var first_name = "asee"
+
+// function myfunc()
+
+// var FirstName;
+// var firstName;
+
+
+// document.getElementById
+
+
+var x = 10;
+var y = 5;
+var z = x + y;
+
+document.write(z)
+document.getElementById('output').innerHTML='output' +z'
+document.getElementById('demo').innerHTML="Hello World"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
